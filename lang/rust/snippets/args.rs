e050dbd9a6e044628e1221930a1a0494a295f81c
sudo-l3wis <68811741+sudo-l3wis@users.noreply.github.com>
2025-04-02 19:50:10 +0100

    Update args.rs

--- lang/rust/snippets/args.rs
+++ lang/rust/snippets/args.rs
@@ -8,3 +8,32 @@ fn main() {
 	}
 }
 
+n main2() {
+    let args: Vec<String> = env::args().collect();
+    if args.contains(&"--help".to_string()) {
+        println!("Usage: feud [--compile, -c] [--run, -r] [--interpret, -i]");
+        return;
+    }
+    let mut debug = false;
+    let mut mode: Option<String> = None;
+    let mut iter = args.iter().peekable();
+    while let Some(arg) = iter.next() {
+        match arg.as_str() {
+            "--verbose" | "-v" => {
+                debug = true;
+            }
+            "-c" | "-r" | "-i" => {
+                mode = Some(arg.clone());
+            }
+            _ => {
+                mode = Some("-i".to_string());
+            }
+        }
+    }
+    match mode.as_deref() {
+        Some("-c") => println!("Compiling..."),
+        Some("-r") => println!("Running..."),
+        Some("-i") => println!("Interpreting..."),
+        _ => println!("Unknown option.")
+    }
+}
